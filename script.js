4f1c944b002e5a47a2069d8b3b077d33f264a940
Gobousei <93245017+Gobousei@users.noreply.github.com>
2023-03-04 19:14:02 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
-
-const url = "https://script.google.com/macros/s/AKfycbz0v2pNU1x628bUqOwKkGvPJTU1tBmu6UV6qnADGtXFK497qX_Ed3RO-QrwaRqB4_we/exec"
+const url = "https://script.google.com/macros/s/AKfycbw5fwPttBRBZCbPatGb615monOctCKtLqA7LojTcWfBDRMkOsFDofrYRhp7Vm7TUVK9LQ/exec"
 fetch(url , {
   method: "GET",
 }).then(response => response.text())
 .then(text => {
-  // 取得した値をコンソールに出力
-  console.log(text);
-  // HTML上の必要な箇所に値を設定します。
-  const targetID = "hoge";
-  document.getElementById(targetID).innerText = text;
+  var res =JSON.parse(text);
+  console.log(res);
+  for(i=0;i<res.length;i++){
+  let tbl = document.getElementById('tbl');
+  
+  let tr = document.createElement('tr');
+  let td = document.createElement('td');
+  var id = i+1;
+  td.id='tit-'+id;
+  td.setAttribute('onclick', 'moreButton(this.id)');
+  td.innerText=res[i][0];
+  td.classList.add("titletd")
+  let goodtd = document.createElement('td');
+  goodtd.innerText='👍'+res[i][1];
+  goodtd.id='good-'+id;
+  goodtd.setAttribute('onclick', 'goodButton(this.id)');
+  goodtd.classList.add("goodtd");
+  tr.appendChild(td);
+  tr.appendChild(goodtd);
+   tbl.appendChild(tr);
+  }
 });
+function goodButton(id){
+  var a =document.getElementById(id).innerText;
+  var n = a.split('👍');
+  var numb = Number(n[1])+1;
+  document.getElementById(id).innerText='👍'+numb;
+  var sp = id.split('-');
+  var te = sp[1];
+  console.log(te);
+  var form = url+"?page=good&id="+te;
+  fetch(form,{
+    method:"GET",
+  }).then(response => response.text())
+  .then(text => {
+ console.log(text);   
+});
+}
+
+function moreButton(id){
+  var sp = id.split('-');
+  var te = sp[1];
+  location.href="./more.html?id="+te;
+}
